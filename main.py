bf7d23b831d2fb33671a4add5dffcceb551a985d
cc484 <32974899+cc484@users.noreply.github.com>
2018-03-08 17:53:43 -0500

    update main

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from brain_state_extraction import brain_st_extr
 
 
 def main():
-    # generate dictionary for subject filenames
+    # generate dictionary for subject and day filenames
     header = 'HOA112/dbrf/'
-    subject = {}
-    for n in range(20):
-        n = n + 1
-        subject[n] = header + 's' + str(n) + '/'
+    subject_dict = {}
+    for s in range(20):
+        s = s + 1
+        subject_dict[s] = header + 's' + str(s) + '/'
+
+    days_dict = {}
+    for d in range(4):
+        d = d + 1
+        days_dict[d] = 'day' + str(d) + '/'
+
+    learn_dict = {}
+    for l in range(4):
+        l = l + 1
+        learn_dict[l] = 'LEARN' + str(l) + '/'
 
     # extracts brain states from all subjects during day1 brain scans
     g_val = 1
-    brain_states_arr = []
-    for s in subject:
-        for i in range(len(subject)):
-            # converts csv to numpy array
-            condition_matrix = np.genfromtxt(subject[s] + 'day1/LEARN1/TS_HOA112.csv', delimiter=',')
+    brain_states_arr = np.empty(len(subject_dict), dtype=tuple())
+    for s, d, l in subject_dict, days_dict, learn_dict
+        # converts csv to numpy array
+        condition_matrix = np.genfromtxt(subject_dict[s] + days_dict[d] + learn_dict[l] +
+                                        'TS_HOA112.csv' , delimiter=',')
+        for i in range(len(subject_dict)):
             # stores returned tuple values from brain_state_extraction in brain_states array
             brain_states_arr[i] = brain_st_extr(condition_matrix, g_val)
 
--- multislice_static_signed.py
+++ multislice_static_signed.py
@@ -5,11 +5,15 @@ from louvain import mod_max
 
 def multislice_stat_si(a, g_plus):
     a_plus = a
-    a_plus[a < 0] = 0
+    for i in range(a_plus.shape[0]):
+        for j in range(a_plus.shape[1]):
+            if a_plus[i, j] < 0:
+                a_plus[i, j] = 0
+
     k_plus = np.sum(a_plus)
     p = np.divide(k_plus * k_plus.T, np.sum(k_plus))
     b = np.subtract(a, np.multiply(p, g_plus))
-    # print(b)
+    print(b)
     b = np.asarray(b)
     print(b)
     b_graph = nx.from_numpy_array(b)
